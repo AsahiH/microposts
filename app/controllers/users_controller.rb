718ef83459566ac7b7f1e8e72a3095291847858e
Asahi Hatta <asahi-aus-hawaii@live.com>
2017-01-06 06:55:33 +0000

    fixed after mentoring Lesson 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,12 +36,14 @@ class UsersController < ApplicationController
   end
   
   def followings
+    @title = "Followings"
     @user  = User.find(params[:id])
     @users = @user.following_users
     render 'show_follow'
   end
 
   def followers
+    @title = "Followers"
     @user  = User.find(params[:id])
     @users = @user.follower_users
     render 'show_follow'
